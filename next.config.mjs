042c9f17b54cd8c8addff16e1cc9cc31895502eb
Zijian Zhong <47649326+kevinz98@users.noreply.github.com>
2024-08-11 22:30:18 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isProd ? '/my-app' : '',
 };
 
 export default nextConfig;
