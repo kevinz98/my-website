92393b813c32d9deccf2efc941af5bc03f4cbf86
Zijian Zhong <47649326+kevinz98@users.noreply.github.com>
2024-08-11 20:51:50 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
